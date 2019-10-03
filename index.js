96166c6391953eb562ec9ded070d23680e27f4b3
hunzsig <mzyhaohaoren@qq.com>
2019-10-03 16:39:54 +0800

    up

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ export {default as Anchor} from './Anchor';
 export {default as I18n} from './I18n';
 export {default as Image} from './Image/Const';
 export {default as Img} from './Image/Img';
+export {default as Picture} from './Image/Picture';
 export {default as Cookie} from './Storage/Cookie';
 export {default as Parse} from './Parse';
 export {default as Path} from './Path';
